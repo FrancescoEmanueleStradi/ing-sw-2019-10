a849731bc1422cf1afa3d74c736cc2507dc19f36
Diego Savoia <diego.savoia@mail.polimi.it>
2019-04-02 16:02:40 +0200

    Added class "Grid"

--- src/main/java/Model/Grid.java
+++ src/main/java/Model/Grid.java
@@ -11,4 +11,29 @@ public class Grid {
     Deck weaponDeck;
     Deck powerupDeck;
     Deck ammoDeck;
+
+    public Grid(int aType) throws InvalidColourException {
+        this.weaponDeck = new WeaponDeck();
+        this. powerupDeck = new PowerUpDeck();
+        this.ammoDeck = new AmmoDeck();
+        this.board = new Board(aType);
+        this.pB = new ArrayList<PlayerBoard>();
+        this.fig = new ArrayList<Figure>();
+    }
+
+    public void addPB(PlayerBoard pb) {
+        this.pB.add(pb);
+    }
+
+    public void removePB(PlayerBoard pb) {
+        this.pB.remove(pb);
+    }
+
+    public void addFIG(Figure fig) {
+        this.fig.add(fig);
+    }
+
+    public void removeFIG(Figure fig) {
+        this.fig.remove(fig);
+    }
 }

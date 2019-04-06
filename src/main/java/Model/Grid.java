9e9985ce2e4a0746ee67cff286d28611ff8aa7f9
FrancescoEmanueleStradi <francescoemanuele.stradi@mail.polimi.it>
2019-04-06 17:44:13 +0200

    added an important comment

--- src/main/java/Model/Grid.java
+++ src/main/java/Model/Grid.java
@@ -86,7 +86,7 @@ public class Grid {
         }
 
     }
-
+    
 
     
 }
--- src/main/java/Model/Position.java
+++ src/main/java/Model/Position.java
@@ -1,7 +1,7 @@
 package Model;
 
 public class Position {
-    private int x;
+    private int x;                    //Remember x is for rows, and y is for columns
     private int y;
 
     public Position(int x, int y){

3a4f5a1c7075b52aa2e1f7ce03bffb7a6fba3bbd
FrancescoEmanueleStradi <francescoemanuele.stradi@mail.polimi.it>
2019-04-08 17:19:57 +0200

    no message

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -1,4 +1,32 @@
 package controller;
 
+import model.cards.*;
+import model.*;
+
 public class Game {
+
+    private Grid grid;
+
+    public void askPlayerDamage(){
+        System.out.println("Write the Nickname of player you want to damage");
+    }
+
+    public void errorInvalidPlayer(){
+        System.out.println("You wrote an Invalid Nickname");
+    }
+
+    public void enterYourNickname(){
+        System.out.println("Enter your Nickname:");
+    }
+
+    public void effectType(){
+        System.out.println("Which type of effect you desire to use?");
+    }
+
+    public void printCardDetails(WeaponCard w){
+        System.out.println("here are the details:"+w.getDescription());
+    }
+
+    
+
 }

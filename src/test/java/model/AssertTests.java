63ea3a012e8afaa34e0bda9c34cc0331abe9823d
Diego Savoia <diego.savoia@mail.polimi.it>
2019-04-12 20:23:39 +0200

    Added tests for classes "PowerUpDeck" and "WeaponDeck"

--- src/test/java/model/AssertTests.java
+++ src/test/java/model/AssertTests.java
@@ -6,10 +6,10 @@ import model.board.KillTrack;
 import model.board.WeaponSlot;
 import model.cards.PowerUpCard;
 import model.cards.WeaponCard;
-import model.cards.powerupcards.Newton;
-import model.cards.powerupcards.TagbackGrenade;
-import model.cards.powerupcards.TargetingScope;
+import model.cards.powerupcards.*;
 import model.cards.weaponcards.*;
+import model.decks.PowerUpDeck;
+import model.decks.WeaponDeck;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -527,5 +527,97 @@ class AssertTests {
         assertEquals("Machine Gun", name);
         assertEquals(Colour.BLUE, colour1.getC());
         assertEquals(Colour.RED, colour2.getC());
+    }
+
+    @Test
+    void PowerUpDeckTest() throws InvalidColourException {
+        PowerUpDeck puDeck = new PowerUpDeck();
+
+        assertEquals("Targeting Scope", puDeck.getTopOfDeck().getCardName());
+        assertEquals(Colour.RED, puDeck.getTopOfDeck().getC());
+        assertEquals("Targeting Scope", puDeck.getDeck().get(1).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(1).getC());
+        assertEquals("Targeting Scope", puDeck.getDeck().get(2).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(2).getC());
+        assertEquals("Targeting Scope", puDeck.getDeck().get(3).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(3).getC());
+        assertEquals("Targeting Scope", puDeck.getDeck().get(4).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(4).getC());
+        assertEquals("Targeting Scope", puDeck.getDeck().get(5).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(5).getC());
+        assertEquals("Newton", puDeck.getDeck().get(6).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(6).getC());
+        assertEquals("Newton", puDeck.getDeck().get(7).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(7).getC());
+        assertEquals("Newton", puDeck.getDeck().get(8).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(8).getC());
+        assertEquals("Newton", puDeck.getDeck().get(9).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(9).getC());
+        assertEquals("Newton", puDeck.getDeck().get(10).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(10).getC());
+        assertEquals("Newton", puDeck.getDeck().get(11).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(11).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(12).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(12).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(13).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(13).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(14).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(14).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(15).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(15).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(16).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(16).getC());
+        assertEquals("Tagback Grenade", puDeck.getDeck().get(17).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(17).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(18).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(18).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(19).getCardName());
+        assertEquals(Colour.RED, puDeck.getDeck().get(19).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(20).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(20).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(21).getCardName());
+        assertEquals(Colour.BLUE, puDeck.getDeck().get(21).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(22).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(22).getC());
+        assertEquals("Teleporter", puDeck.getDeck().get(23).getCardName());
+        assertEquals(Colour.YELLOW, puDeck.getDeck().get(23).getC());
+
+        Newton fakeCard = new Newton(Colour.RED);
+
+        puDeck.addCard(fakeCard);
+        assertEquals(fakeCard, puDeck.getDeck().get(24));
+    }
+
+
+    @Test
+    void WeaponDeckTest() throws InvalidColourException {
+        WeaponDeck wDeck = new WeaponDeck();
+
+        assertEquals("Cyberblade", wDeck.getDeck().get(0).getCardName());
+        assertEquals("Electroscythe", wDeck.getDeck().get(1).getCardName());
+        assertEquals("Flamethrower", wDeck.getDeck().get(2).getCardName());
+        assertEquals("Furnace", wDeck.getDeck().get(3).getCardName());
+        assertEquals("Grenade Launcher", wDeck.getDeck().get(4).getCardName());
+        assertEquals("Heatseeker", wDeck.getDeck().get(5).getCardName());
+        assertEquals("Hellion", wDeck.getDeck().get(6).getCardName());
+        assertEquals("Lock Rifle", wDeck.getDeck().get(7).getCardName());
+        assertEquals("Machine Gun", wDeck.getDeck().get(8).getCardName());
+        assertEquals("Plasma Gun", wDeck.getDeck().get(9).getCardName());
+        assertEquals("Power Glove", wDeck.getDeck().get(10).getCardName());
+        assertEquals("Railgun", wDeck.getDeck().get(11).getCardName());
+        assertEquals("Rocket Launcher", wDeck.getDeck().get(12).getCardName());
+        assertEquals("Shockwave", wDeck.getDeck().get(13).getCardName());
+        assertEquals("Shotgun", wDeck.getDeck().get(14).getCardName());
+        assertEquals("Sledgehammer", wDeck.getDeck().get(15).getCardName());
+        assertEquals("T.H.O.R.", wDeck.getDeck().get(16).getCardName());
+        assertEquals("Tractor Beam", wDeck.getDeck().get(17).getCardName());
+        assertEquals("Vortex Cannon", wDeck.getDeck().get(18).getCardName());
+        assertEquals("Whisper", wDeck.getDeck().get(19).getCardName());
+        assertEquals("ZX-2", wDeck.getDeck().get(20).getCardName());
+
+        WeaponCard fakeCard = new MachineGun();
+
+        wDeck.addCard(fakeCard);
+        assertEquals("Machine Gun", wDeck.getDeck().get(21).getCardName());
     }
 }

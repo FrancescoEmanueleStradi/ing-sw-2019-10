b527b9086ef84173a3738e9e2839b2749767c6a7
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-04-14 16:56:25 +0200

    Begin implementing RMI

---
+++ src/main/java/controller/Client.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.util.*;
+import javax.naming.*;
+
+public class Client {
+
+    public static void main(String args[]) throws NamingException, RemoteException, AlreadyBoundException {
+        //ServerInterface svr
+    }
+}

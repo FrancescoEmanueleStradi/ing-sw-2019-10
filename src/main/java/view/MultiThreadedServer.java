c82de5f1c4f95f3e13afbf335431f0010ad507a9
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-04-26 17:34:47 +0200

    Extended class "MultiThreadedServer"

--- src/main/java/view/MultiThreadedServer.java
+++ src/main/java/view/MultiThreadedServer.java
@@ -1,4 +1,18 @@
 package view;
 
+import java.net.ServerSocket;
+import java.util.concurrent.*;
+
 public class MultiThreadedServer {
+
+    private int portNum;
+
+    public MultiThreadedServer(int port) {
+        portNum = port;
+    }
+
+    public void startServer() {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        ServerSocket socket;
+    }
 }

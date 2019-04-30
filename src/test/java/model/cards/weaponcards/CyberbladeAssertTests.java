3ed812e54898a754a748758457bd484c7aa6cca8
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-04-30 10:13:27 +0200

    Finished Cyberblade tests

--- src/test/java/model/cards/weaponcards/CyberbladeAssertTests.java
+++ src/test/java/model/cards/weaponcards/CyberbladeAssertTests.java
@@ -3,6 +3,7 @@ package model.cards.weaponcards;
 import model.Colour;
 import model.Grid;
 import model.InvalidColourException;
+import model.Position;
 import model.cards.WeaponCard;
 import model.player.Player;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,11 @@ class CyberbladeAssertTests {
         assertEquals("Cyberblade", c.getCardName());
         assertEquals(Colour.YELLOW, c.getReloadCost()[0].getC());
         assertEquals(Colour.RED, c.getReloadCost()[1].getC());
-        assertEquals("basic effect: Deal 2 damage and 1 mark to 1 target you can see.\n" +
-                        "with second lock: Deal 1 mark to a different target you can see.\n",
+        assertEquals("basic effect: Deal 2 damage to 1 target on your square.\n" +
+                "with shadowstep: Move 1 square before or after the basic effect.\n" +
+                "with slice and dice: Deal 2 damage to a different target on your square.\n" +
+                "The shadowstep may be used before or after this effect.\n" +
+                "Notes: Combining all effects allows you to move onto a square and whack 2 people; or whack somebody, move, and whack somebody else; or whack 2 people and then move.\n",
                 c.getDescription());
 
         assertEquals(2, c.getNumOptionalEffect());
@@ -38,17 +42,28 @@ class CyberbladeAssertTests {
         assertEquals("Slice and Dice", c.getOptionalEffect2());
 
         Grid grid = new Grid();
-        Player player = new Player("Myself", Colour.YELLOW, true);
-        Player enemy1 = new Player("Enemy 1", Colour.BLACK, false);
-        Player enemy2 = new Player("Enemy 2", Colour.RED, false);
-        String x = "2";
-        String y = "1";
+        grid.setType(1);
 
+        Player player = new Player("Myself", Colour.YELLOW, true);
+        Position oldPos = new Position(0,1);
+        grid.move(player, oldPos);
+
+        Player enemy1 = new Player("Enemy 1", Colour.BLACK, false);
         c.applyEffect(grid, player, enemy1);
         assertEquals(Colour.YELLOW, enemy1.getpB().getDamages().getDamageTr()[1].getC());
 
-        //TODO
+        String x = "0";
+        String y = "2";
         c.applySpecialEffect(grid, player, x, y);
+        assertTrue(grid.distance(player, oldPos) == 1);
 
+        Player enemy2 = new Player("Enemy 2", Colour.RED, false);
+        Position enemyPos = new Position(0,2);
+        grid.move(enemy2, enemyPos);
+        //grid.moveInMyCell doesn't work
+        assertEquals(player.getCell(), enemy2.getCell());
+
+        c.applySpecialEffect2(grid, player, enemy2);
+        assertEquals(Colour.YELLOW, enemy2.getpB().getDamages().getDamageTr()[1].getC());
     }
 }

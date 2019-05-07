42f3f6a45a04d66f8ac5f670a8c1b675e1a48d53
Diego Savoia <diego.savoia@mail.polimi.it>
2019-05-07 16:03:13 +0200

    Fixed signature

--- src/main/java/view/ServerInterface.java
+++ src/main/java/view/ServerInterface.java
@@ -41,50 +41,50 @@ public interface ServerInterface extends Remote {
     List<PowerUpCard> messageGiveTwoPUCard(int game, String nick) throws RemoteException;       //TODO Problem
     boolean messageIsValidPickAndDiscard(int game, String nick) throws RemoteException;
     void messagePickAndDiscardCard(int game, String nick, PowerUpCard p1, PowerUpCard p2) throws RemoteException;
-    boolean messageIsValidFirstActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP) throws RemoteException;
-    void messageFirstActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidFirstActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageFirstActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     boolean messageIsValidFirstActionMove(int game, String nick, List<Integer> d) throws RemoteException;
     void messageFirstActionMove(int game, String nick, List<Integer> d) throws RemoteException;
     List<String> messageGetWeaponCardLoaded(int game, String nick) throws RemoteException;      //TODO Problem
     boolean messageIsValidCard(int game, String nick, String weaponCard) throws RemoteException;
     List<Colour> messageGetReloadCost(int game, String s, String nick) throws RemoteException;      //TODO Problem
     String messageGetDescriptionWC(int game, String s, String nick) throws RemoteException;     //TODO Problem
-    boolean messageIsValidFirstActionGrab(int game, String nick, Integer[] d, String wC, String wS, List<Colour> lA, List<String> lP) throws RemoteException;
-    void messageFirstActionGrab(int game, String nick, Integer[] d, String wC, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidFirstActionGrab(int game, String nick, Integer[] d, String wC, String wS, List<Colour> lA, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageFirstActionGrab(int game, String nick, Integer[] d, String wC, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     boolean messageIsDiscard(int game) throws RemoteException;
     void messageDiscardWeaponCard(int game, String nick, String wS, String wC) throws RemoteException;
-    boolean messageIsValidSecondActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP) throws RemoteException;
-    void messageSecondActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidSecondActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageSecondActionShoot(int game, String nick, String wC, List<Integer> lI, List<String> lS, int d, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     boolean messageIsValidSecondActionMove(int game, List<Integer> d) throws RemoteException;
     void messageSecondActionMove(int game, String nick, List<Integer> d) throws RemoteException;
-    boolean messageIsValidSecondActionGrab(int game, String nick, Integer[] d, String wC, String wS, List<Colour> lA, List<String> lP) throws RemoteException;
-    void messageSecondActionGrab(int game, String nick, Integer[] d, String wC, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidSecondActionGrab(int game, String nick, Integer[] d, String wC, String wS, List<Colour> lA, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageSecondActionGrab(int game, String nick, Integer[] d, String wC, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     List<String> messageGetPowerUpCard(int game, String nick) throws RemoteException;       //TODO Problem
-    String messageGetDescriptionPUC(int game, String pC, String nick) throws RemoteException;       //TODO Problem
-    boolean messageIsValidUsePowerUpCard(int game, String nick, String pC, List<String> l, Colour c) throws RemoteException;
-    void messageUsePowerUpCard(int game, String nick, String pC, List<String> l, Colour c) throws RemoteException;
+    String messageGetDescriptionPUC(int game, String pC, String col, String nick) throws RemoteException;       //TODO Problem
+    boolean messageIsValidUsePowerUpCard(int game, String nick, String pC, String col, List<String> l,Colour c) throws RemoteException;
+    void messageUsePowerUpCard(int game, String nick, String pC, String col, List<String> l, Colour c) throws RemoteException;
     List<String> messageGetWeaponCardUnloaded(int game, String nick) throws RemoteException;        //TODO Problem
     boolean messageIsValidReload(int game, String nick, String s) throws RemoteException;
     void messageReload(int game, String nick, String s, int end) throws RemoteException;
     boolean messageIsValidScoring(int game) throws RemoteException;
     void messageScoring(int game) throws RemoteException;
     List<String> messageGetDeadList(int game) throws RemoteException;       //TODO Problem
-    boolean messageIsValidDiscardCardForSpawnPoint(int game, String nick, String s) throws RemoteException;
-    void messageDiscardCardForSpawnPoint(int game, String nick, String s) throws RemoteException;
+    boolean messageIsValidDiscardCardForSpawnPoint(int game, String nick, String s, String c) throws RemoteException;
+    void messageDiscardCardForSpawnPoint(int game, String nick, String s, String c) throws RemoteException;
     boolean messageIsValidToReplace(int game) throws RemoteException;
     void messageReplace(int game) throws RemoteException;
     boolean messageIsValidFinalFrenzyAction(int game, String nick, List<String> l) throws RemoteException;
     List<String> messageGetWeaponCard(int game, String nick) throws RemoteException;        //TODO Problem
-    boolean messageIsValidFinalFrenzyAction1(int game, String nick, int d, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP) throws RemoteException;
-    void messageFinalFrenzyAction1(int game, String nick, int d, List<String> lW, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidFinalFrenzyAction1(int game, String nick, int d, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageFinalFrenzyAction1(int game, String nick, int d, List<String> lW, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     boolean messageIsValidFinalFrenzyAction2(int game, String nick, List<Integer> d) throws RemoteException;
     void messageFinalFrenzyAction2(int game, String nick, List<Integer> d) throws RemoteException;
-    boolean messageIsValidFinalFrenzyAction3(int game, String nick, List<Integer> d, String wC, String wS, List<Colour> lC, List<String> lP) throws RemoteException;
-    void messageFinalFrenzyAction3(int game, String nick, List<Integer> d, String wC, List<Colour> lC, List<String> lP) throws RemoteException;
-    boolean messageIsValidFinalFrenzyAction4(int game, String nick, List<Integer> d, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP) throws RemoteException;
-    void messageFinalFrenzyAction4(int game, String nick, List<Integer> d, List<String> lW, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP) throws RemoteException;
-    boolean messageIsValidFinalFrenzyAction5(int game, String nick, List<Integer> d, String wC, String wS, List<Colour> lC, List<String> lS) throws RemoteException;
-    void messageFinalFrenzyAction5(int game, String nick, List<Integer> d, String wC, List<Colour> lC, List<String> lP) throws RemoteException;
+    boolean messageIsValidFinalFrenzyAction3(int game, String nick, List<Integer> d, String wC, String wS, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageFinalFrenzyAction3(int game, String nick, List<Integer> d, String wC, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    boolean messageIsValidFinalFrenzyAction4(int game, String nick, List<Integer> d, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    void messageFinalFrenzyAction4(int game, String nick, List<Integer> d, List<String> lW, String wC, List<Integer> lI, List<String> lS, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
+    boolean messageIsValidFinalFrenzyAction5(int game, String nick, List<Integer> d, String wC, String wS, List<Colour> lC, List<String> lS, List<String> lPC) throws RemoteException;
+    void messageFinalFrenzyAction5(int game, String nick, List<Integer> d, String wC, List<Colour> lC, List<String> lP, List<String> lPC) throws RemoteException;
     void messageFinalFrenzyTurnScoring(int game) throws RemoteException;
     void messageEndTurnFinalFrenzy(int game) throws RemoteException;
     void messageFinalScoring(int game) throws RemoteException;

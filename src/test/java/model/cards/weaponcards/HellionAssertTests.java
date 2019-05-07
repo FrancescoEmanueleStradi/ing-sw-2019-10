c21e2e6cd366c13a2181c62429f5897f796aab95
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-05-07 15:35:04 +0200

    Added tests

---
+++ src/test/java/model/cards/weaponcards/HellionAssertTests.java
@@ -0,0 +1,25 @@
+package model.cards.weaponcards;
+
+import model.Colour;
+import model.cards.WeaponCard;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HellionAssertTests {
+    @Test
+    void HellionCorrectConstructor() {
+        WeaponCard h = new Hellion();
+
+        assertEquals("Hellion", h.getCardName());
+        assertEquals(Colour.RED, h.getReloadCost()[0].getC());
+        assertEquals(Colour.YELLOW, h.getReloadCost()[1].getC());
+        assertEquals("basic mode: Deal 1 damage to 1 target you can see at least 1 move away. Then give 1 mark to that target and everyone else on that square.\n" +
+                        "in nano-tracer mode: Deal 1 damage to 1 target you can see at least 1 move away. Then give 2 marks to that target and everyone else on that square.\n",
+                h.getDescription());
+
+        assertEquals(0, h.getNumOptionalEffect());
+        assertTrue(h.hasAlternateFireMode());
+    }
+}

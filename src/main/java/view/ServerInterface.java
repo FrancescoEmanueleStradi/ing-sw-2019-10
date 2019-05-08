3a2ca685c298d246ff2f3452b46b54a6b24172ed
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-05-08 16:02:57 +0200

    More CLI changes

--- src/main/java/view/ServerInterface.java
+++ src/main/java/view/ServerInterface.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 
 public interface ServerInterface extends Remote {
-    
+
     String echo(String input) throws RemoteException;
     int getGames() throws RemoteException;
     int setGame(int game) throws RemoteException;

9fb6f8ad4da36482c8bc6b7444ca2371b9832650
Diego Savoia <diego.savoia@mail.polimi.it>
2019-05-14 16:16:05 +0200

    Minor changes

--- src/main/java/controller/Game.java
+++ src/main/java/controller/Game.java
@@ -478,11 +478,11 @@ public class Game {                                 //Cli or Gui -- Rmi or Socke
                            x = true;
                    }
                    else if(lI.size() == 2 && lI.get(0) == 1 && lI.get(1) == 2 && lS.size() > 1) {
-                       if(this.grid.isInViewZone(p, this.grid.getPlayerObject(lS.get(0))) && this.grid.isInViewZone(this.grid.getPlayerObject(lS.get(0)), this.grid.getPlayerObject(lS.get(1))) && !this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(1))) && lC.contains(Colour.BLUE))
+                       if(this.grid.isInViewZone(p, this.grid.getPlayerObject(lS.get(0))) && this.grid.isInViewZone(this.grid.getPlayerObject(lS.get(0)), this.grid.getPlayerObject(lS.get(1))) && !(this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(1)))) && lC.contains(Colour.BLUE))
                            x = true;
                    }
                    else if(lI.size() == 3 && lI.get(0) == 1 && lI.get(1) == 2 && lI.get(2) == 3 && lS.size() > 2 && this.grid.isInViewZone(p, this.grid.getPlayerObject(lS.get(0))) && this.grid.isInViewZone(this.grid.getPlayerObject(lS.get(0)), this.grid.getPlayerObject(lS.get(1))) && this.grid.isInViewZone(this.grid.getPlayerObject(lS.get(1)), this.grid.getPlayerObject(lS.get(2))) &&
-                               !this.grid.getPlayerObject(lS.get(1)).equals(this.grid.getPlayerObject(lS.get(2))) && !this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(2))) && !this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(1))) && lC.containsAll(Arrays.asList(Colour.BLUE, Colour.BLUE)))
+                               !(this.grid.getPlayerObject(lS.get(1)).equals(this.grid.getPlayerObject(lS.get(2)))) && !(this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(2)))) && !(this.grid.getPlayerObject(lS.get(0)).equals(this.grid.getPlayerObject(lS.get(1)))) && lC.containsAll(Arrays.asList(Colour.BLUE, Colour.BLUE)))
                            x = true;
                    break;
                case "Tractor Beam":

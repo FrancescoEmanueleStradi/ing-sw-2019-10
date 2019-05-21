d1cf61b58ada769295c0a4f3650d67e1157c1482
Diego Savoia <diego.savoia@mail.polimi.it>
2019-05-21 16:56:52 +0200

    Fixed problem with gameState

--- src/main/java/view/CLI.java
+++ src/main/java/view/CLI.java
@@ -874,8 +874,10 @@ public class CLI extends UnicastRemoteObject implements View {
 
     @Override
     public void scoring() throws RemoteException{
-        if(this.server.messageIsValidScoring(game))
+        if(this.server.messageIsValidScoring(game)) {
+            System.out.println("Scoring...");
             this.server.messageScoring(game);
+        }
         else
             System.out.println("Not scoring");
     }
@@ -900,10 +902,15 @@ public class CLI extends UnicastRemoteObject implements View {
 
     @Override
     public void replace() throws RemoteException{
-        if(this.server.messageIsValidToReplace(game))
+        if(this.server.messageIsValidToReplace(game)) {
+            System.out.println("Replacing...");
             this.server.messageReplace(game);
-        else
+            System.out.println("Your turn is ended");
+        }
+        else {
             System.out.println("Not replacing");
+            System.out.println("Your turn is ended");
+        }
     }
 
 

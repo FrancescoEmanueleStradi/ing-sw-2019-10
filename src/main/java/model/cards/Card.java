135cd8907e0bffbfe860736ba8ecc4c21a2b258e
Alexander Elvis Stanimirov <10562605@polimi.it>
2019-06-19 13:22:31 +0200

    Started cleaning up code

--- src/main/java/model/cards/Card.java
+++
@@ -1,6 +0,0 @@
-package model.cards;
-
-public interface Card {
-      //void applyEffect(Grid grid, Player p, Player p1); //player attacks p1   (useful? almost every card has a different signature)
-
-}

e248e2efc41fb2b25c32cf6db816b3abc409a94b
FrancescoEmanueleStradi <francescoemanuele.stradi@mail.polimi.it>
2019-07-02 18:43:32 +0200

    Working on NewSpawPoint and Reload in GUI

--- src/main/java/view/gui/ReloadPanel.java
+++ src/main/java/view/gui/ReloadPanel.java
@@ -1,36 +1,63 @@
 package view.gui;
 
+import network.ServerInterface;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
+import java.util.LinkedList;
 
 public class ReloadPanel extends JPanel implements ActionListener {
 
-    private JButton b;
-    private JTextField txt1;
-    private JTextField txt2;
-    private JTextField txt3;
+    private GUI gui;
+    private ServerInterface server;
+    private JFrame parent;
+    private int game;
+    private String nickName;
 
-    public ReloadPanel() {
+    public ReloadPanel(GUI gui, ServerInterface server, JFrame parent, int game, String nickName) throws RemoteException {
         super();
-        add(new JLabel("Click on the name of the card you want to discard, or on exit if you have finished"));
+        this.gui = gui;
+        this.server = server;
+        this.parent = parent;
+        this.game = game;
+        this.nickName = nickName;
+
+        add(new JLabel("Click on the name of the card you want to load, or on exit if you have finished"));
         add(new JButton("exit"));
+
+        for (String s : this.server.messageGetWeaponCardUnloaded(game, this.nickName)) {
+
+            this.add(new JButton(s));
+        }
+
     }
 
-
-
-    public void addButton(String name) {
-        add(new JButton(name));
-    }
 
 
     public void actionPerformed(ActionEvent e) {
-        /*try {
+        try {
+            String action = e.getSource().toString();
+
+            if(action.equals("exit")) {
+                gui.reload();
+            }
+
+            else{
+                if(this.server.messageIsValidReload(game, this.nickName, action))
+                    this.server.messageReload(game, this.nickName, action);
+                gui.reload();
+            }
+
+            parent.setVisible(false);
+            parent.dispose();
 
 
         }catch(RemoteException ex) {
-            //TODO?
-        }*/
+
+        }catch (InterruptedException exc){
+
+        }
     }
 }
